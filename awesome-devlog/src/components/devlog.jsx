d542609be4b699abf916d4e418c1258110e6e1e4
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-27 14:30:58 +0530

    url redirect implemented

--- awesome-devlog/src/components/devlog.jsx
+++ awesome-devlog/src/components/devlog.jsx
@@ -6,15 +6,14 @@ const Devlog = () => {
   return (
     <div className="p-4">
       {devlogs.map((devlog) => (
-        <ul
-          key={devlog.id}
-          className="mb-4 p-4 border rounded shadow-sm bg-stone-600 transition duration-300 ease-in-out transform hover:bg-zinc-500 hover:translate-y-1"
-        >
-          <li className="font-semibold text-slate-100 text-lg mb-2">
-            Game: {devlog.gameName}
-          </li>
-          <li className="text-gray-300">Dev: {devlog.gameDev}</li>
-        </ul>
+        <a href={devlog.url} key={devlog.id}>
+          <ul className="mb-4 p-4 border rounded shadow-sm bg-stone-600 transition duration-300 ease-in-out transform hover:bg-zinc-500 hover:translate-y-1">
+            <li className="font-semibold text-slate-100 text-lg mb-2">
+              Game: {devlog.gameName}
+            </li>
+            <li className="text-gray-300">Dev: {devlog.gameDev}</li>
+          </ul>
+        </a>
       ))}
     </div>
   )
