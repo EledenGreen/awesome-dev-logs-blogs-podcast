a14eed143ca315213ffb9080937425826af05539
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-28 11:02:44 +0530

    updated clickable area to allign

--- awesome-devlog/src/components/devlog.jsx
+++ awesome-devlog/src/components/devlog.jsx
@@ -14,20 +14,22 @@ const Devlog = () => {
   return (
     <div className="p-4">
       {devlogs.map((devlog) => (
-        <a
-          href={devlog.url}
-          key={devlog.id}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <ul className="custom-devlog-width mx-auto mb-4 p-2 border rounded shadow-sm bg-stone-600 transition duration-300 ease-in-out transform hover:bg-zinc-500 hover:translate-y-1">
-            <li className="font-semibold text-slate-100 text-lg mb-2">
-              {devlog.name}
-            </li>
-            <li className="text-gray-300">Dev: {devlog.dev}</li>
-            <li className="text-gray-300">Type: {devlog.type}</li>
-          </ul>
-        </a>
+        <div key={devlog.id} className="custom-devlog-width mx-auto mb-4">
+          <a
+            href={devlog.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block mb-4 p-2"
+          >
+            <ul className="p-2 border rounded shadow-sm bg-stone-600 transition duration-300 ease-in-out transform hover:bg-zinc-500 hover:translate-y-1">
+              <li className="font-semibold text-slate-100 text-lg mb-2">
+                {devlog.name}
+              </li>
+              <li className="text-gray-300">Dev: {devlog.dev}</li>
+              <li className="text-gray-300">Type: {devlog.type}</li>
+            </ul>
+          </a>
+        </div>
       ))}
     </div>
   )
