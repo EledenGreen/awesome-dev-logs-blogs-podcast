20ea68ddf23d8d69ecc494713e6af7b8852969c5
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-28 12:55:44 +0530

    models created

---
+++ Backend/models/devlog.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const devSchema = new mongoose.Schema({
+  name: String,
+  dev: String,
+  type: String,
+  genre: String,
+  url: String,
+})
+
+devSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Dev', devSchema)
